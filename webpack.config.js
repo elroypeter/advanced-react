61efb18e95dd9aea6b575577b3d8ebcb60fd962f
elroypeter <30926117+elroypeter@users.noreply.github.com>
2022-06-21 15:27:58 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./lib/components/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname, "example"),
+            },
+            {
+                directory: path.join(__dirname, "public"),
+            },
+        ],
+        port: 4000,
+        liveReload: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" },
+                ],
+            },
+            {
+                test: /\.(png|jpeg|jpg|svg)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
